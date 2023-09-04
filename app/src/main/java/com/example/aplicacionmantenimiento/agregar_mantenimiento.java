c57ecba3d5bb9cca8325913900482309fa9f5bdf
victo <victorsanmartin038@gmail.com>
2023-09-04 16:16:44 -0300

    version2

---
+++ app/src/main/java/com/example/aplicacionmantenimiento/agregar_mantenimiento.java
@@ -0,0 +1,14 @@
+package com.example.aplicacionmantenimiento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class agregar_mantenimiento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_mantenimiento);
+    }
+}
